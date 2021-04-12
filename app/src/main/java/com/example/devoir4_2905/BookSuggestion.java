f461c8d3dc774eafd4ae27da69781c8f27c73af4
yuyin ding <you@example.com>
2021-04-12 14:42:47 -0400

    Initial 2

--- app/src/main/java/com/example/devoir4_2905/BookSuggestion.java
+++ app/src/main/java/com/example/devoir4_2905/BookSuggestion.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AudioBook extends AppCompatActivity {
+public class BookSuggestion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_audiobook);
+        setContentView(R.layout.activity_book_suggestion);
     }
 }

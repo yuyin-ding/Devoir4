f95e6ed69da51bca3ffca19ac45c24dd6b93843c
yuyin ding <you@example.com>
2021-04-12 21:06:39 -0400

    Layouts et Appel

---
+++ app/src/main/java/com/example/devoir4_2905/Appel.java
@@ -0,0 +1,14 @@
+package com.example.devoir4_2905;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Appel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appel);
+    }
+}

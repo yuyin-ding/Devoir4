bf3844df5c3dcf6e445e918d420917b65ba4aea0
yuyin ding <you@example.com>
2021-04-23 08:41:05 -0400

    Creation des fichiers Java de Plus Acceuil BiblioHistory Decouvrir et Favoris

---
+++ app/src/main/java/com/example/devoir4_2905/Acceuil.java
@@ -0,0 +1,16 @@
+package com.example.devoir4_2905;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Acceuil extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceuil);
+    }
+}
+
---
+++ app/src/main/java/com/example/devoir4_2905/Favoris.java
@@ -0,0 +1,14 @@
+package com.example.devoir4_2905;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Favoris extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favoris);
+    }
+}
